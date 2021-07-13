8ece9eaf56287dd60947a1aabfb5b12c8e413840
artem <artem-prokofiev@yandex.ru>
2021-07-13 13:11:14 +0300

    Added fireball for enemy

--- Assets/Scripts/Enemy/WanderingAI.cs
+++ Assets/Scripts/Enemy/WanderingAI.cs
@@ -7,6 +7,8 @@ public class WanderingAI : MonoBehaviour
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
     public bool _alive; //variable for check consist life or dead
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
 
      void Start()
     {
@@ -25,7 +27,17 @@ public class WanderingAI : MonoBehaviour
             
             if (Physics.SphereCast(ray, 0.75f, out hit)) //we throw a ray with a circle described around it
             {
-                if (hit.distance < obstacleRange) 
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if (_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obstacleRange) 
                 {
                     float angle = Random.Range(-110, 110); //turn with half random direction
                     transform.Rotate(0, angle, 0);
